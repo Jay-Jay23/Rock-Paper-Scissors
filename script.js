84f929ddb0c2cdb88a218ee9e150d0dddc383f72
Jay-Jay23 <skosanajojo@gmail.com>
2023-07-11 14:02:30 +0200

    Add game function to play 5 games while recording score

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function getComputerChoice() {
   const words = ["rock", "paper", "scissors"];
-  let randomPos = Math.floor(Math.random() * words.length);
-  let rndWord = words[randomPos];
-
-  console.log(rndWord);
+  let randomPosition = Math.floor(Math.random() * words.length);
+  let rndWord = words[randomPosition];
 
   return rndWord;
 }
-
-getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
   const player = playerSelection.toLowerCase();
@@ -18,7 +14,7 @@ function playRound(playerSelection, computerSelection) {
     return "You Win! " + player + " beats " + computer;
   } else if (player == "rock" && computer == "scissors") {
     return "You Win! " + player + " beats " + computer;
-  } else if (player === "paper" && computer === "rock") {
+  } else if (player == "paper" && computer == "rock") {
     return "You Win! " + player + " beats " + computer;
   } else if (player == computer) {
     return "It's a draw";
@@ -27,7 +23,39 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const userChoice = prompt("Choose Between Rock, Paper, or Scissors");
-const results = playRound(userChoice, getComputerChoice());
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-console.log(results);
+  for (let round = 1; round <= 5; round++) {
+    const userChoice = prompt(
+      "Choose Between Rock, Paper, or Scissors" + " Round " + round
+    );
+    const results = playRound(userChoice, getComputerChoice());
+    console.log(results);
+
+    if (results.startsWith("You Win! ")) {
+      playerScore++;
+    } else if (results.startsWith("You Lose! ")) {
+      computerScore++;
+    }
+
+    console.log("Scoreboard: " + playerScore + " VS " + computerScore);
+  }
+
+  if (playerScore > computerScore) {
+    console.log(
+      "Player Wins!!!" + " Final: " + playerScore + " VS " + computerScore
+    );
+  } else if (computerScore > playerScore) {
+    console.log(
+      "Computer Wins!!!" + " Final: " + playerScore + " VS " + computerScore
+    );
+  } else {
+    console.log(
+      "Game Tied!!!" + " Final: " + playerScore + " VS " + computerScore
+    );
+  }
+}
+
+game();
